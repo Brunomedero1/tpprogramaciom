f910d3a30ab03edeaeee177cfac725afb9bab253
MateoMoyano <136364033+MateoMoyano@users.noreply.github.com>
2023-08-16 11:38:11 -0300

    Update java.js
    GG EZv2

--- tp2 ejercisio 4 java/java.js
+++ tp2 ejercisio 4 java/java.js
@@ -1,16 +1,4 @@
-
-//
-var lado1 = prompt("ingresar cuanto mide un lado del cuadrado");
-var operacion = prompt("que desea realizar? P=perimetro, A=area/superficie");
-
-
-if(operacion == "A") {
-    area(lado1)
-}
-if(operacion == "P") {
-    perimetro(lado1)
-}
-
+var whi = "SI"
 function area(lado1) {
     var total = lado1 * lado1
     document.write("el area del cuadrado es "+ total);
@@ -19,3 +7,18 @@ function perimetro(lado1) {
     var total2 = parseInt(lado1) + parseInt(lado1) + parseInt(lado1) + parseInt(lado1) 
     document.write("el perimetro del cuadrado es " + total2);
 }
+do {
+    var lado1 = prompt("ingresar cuanto mide un lado del cuadrado");
+    var operacion = prompt("que desea realizar? P=perimetro, A=area/superficie");
+
+
+    if(operacion == "A") {
+    area(lado1)
+    }
+    if(operacion == "P") {
+    perimetro(lado1)
+    }
+
+    var whi  = prompt("desea realisar otra operacion? SI, NO");
+
+} while (whi =="SI");
