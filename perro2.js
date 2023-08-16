202c07758db37391a3048009ecdb190f422e8dd7
brunomedero <81205913+Brunomedero1@users.noreply.github.com>
2023-08-16 11:26:06 -0300

    tp 2 java scrip

---
+++ perro2.js
@@ -0,0 +1,8 @@
+function area(num1,num2){
+total=parseFloat(num1)*parseFloat(num2)
+return total;
+}
+var base = prompt("ingrese base")
+var altura = prompt ("ingrese altura ")
+total=area(base,altura)
+document.write(total)
