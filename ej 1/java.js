31ca2b0ed30361a45b0a391e76f1a84ecb50869c
joaPaez <135851520+joaPaez@users.noreply.github.com>
2023-08-23 15:32:56 -0300

    Add files via upload

---
+++ ej 1/java.js
@@ -0,0 +1,7 @@
+function suma(num1,num2){
+    suma=(num1+num2);
+}
+var num1=parseInt(prompt("ingrese un numero"));
+var num2=parseInt(prompt("ingrese otro numero"));
+suma(num1,num2);
+document.write("el resultado es:"+suma);
